ab25f03e4a2e370bc8b839cc9f3adc66f62b591d
uwap <lx1996@web.de>
2015-05-25 00:56:30 +0200

    Build coffeescripts when running 'cabal build'

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,21 @@
+module Main where
+
 import Distribution.Simple
-main = defaultMain
+import System.Process
+import Control.Monad
+import System.FilePath
+import System.Directory
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+      { buildHook = \pkg lbi hooks flags -> do
+          compileCoffeeScripts
+          buildHook simpleUserHooks pkg lbi hooks flags
+      }
+
+compileCoffeeScripts :: IO ()
+compileCoffeeScripts = do
+  let scriptsDir = "." </> "Web" </> "scripts"
+  files <- getDirectoryContents scriptsDir
+  forM_ files $ \file -> when (takeExtension file == ".coffee") $
+    void $ readProcess "coffee" ["-b", "-c", scriptsDir </> file] ""
