f31fe6e31b23ec3f0aad2822892b3b5c66fa8585
uwap <lx1996@web.de>
2015-05-26 12:30:33 +0200

    Added a variable that sets the amount of pixels in the Animation.hs

--- src/Animation.hs
+++ src/Animation.hs
@@ -11,6 +11,9 @@ import System.IO.Unsafe
 
 import qualified Proto as P
 
+pixels :: Int
+pixels = 30
+
 type Frame = [Color]
 type AnimationGenerator = Integer -> Frame -> IO Frame
 type Animation = [AnimationGenerator]
@@ -39,7 +42,7 @@ fill :: Frame -> Animation
 fill frame = return $ const $ const $ return frame
 
 fillColor :: Color -> Animation
-fillColor color = return $ const $ const $ return $ replicate 30 color
+fillColor color = return $ const $ const $ return $ replicate pixels color
 
 cycleLeft :: Animation
 cycleLeft = return (\i -> step (i `mod` 300))
@@ -58,11 +61,11 @@ cycleRight = return (\i -> step (i `mod` 300))
 centerColor :: Color -> Animation
 centerColor color = return step
   where 
-    step _ _ = return $ center color 30
+    step _ _ = return $ center color pixels
     
     center :: Color -> Int -> Frame
     center col 0 = [(0,0,0)]
-    center col i = (fromIntegral ((15 - abs(15 - i)) ^ 2) * col) : center col (i -1)
+    center col i = (fromIntegral ((round(fromIntegral pixels / 2) - abs(round(fromIntegral pixels / 2) - i)) ^ 2) * col) : center col (i -1)
 
 fillRandom :: Animation
 fillRandom = return step
@@ -75,11 +78,11 @@ sinBrightness :: Animation
 sinBrightness = return step
   where
     step i frame = let factor = 1 - abs (sin ((fromIntegral i / 100) + 3.1415926535/2)) in
-      return $ fmap (uncurry (*)) $ zip frame $ flip setBrightness factor <$> replicate 30 (255,255,255)
+      return $ fmap (uncurry (*)) $ zip frame $ flip setBrightness factor <$> replicate pixels (255,255,255)
 
 addSin :: Int -> Color -> Animation
 addSin i col = return step
   where
     step j frame = let factor = 1 - abs (sin ((fromIntegral i / 100) + 3.1415926535/2)) in
       return $ fmap (uncurry (+)) $ zip frame $ flip setBrightness factor <$> createSin
-    createSin = (*col) <$> fromIntegral <$> [(min (3-abs(i - j)) 0) * 80 | j <- [1..30]]
+    createSin = (*col) <$> fromIntegral <$> [(min (3-abs(i - j)) 0) * 80 | j <- [1..pixels]]
